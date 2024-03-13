d0644f03be6e02086ebfae58d49bc443ff65ac7a
Netanel Harush <87550014+NetanelH2@users.noreply.github.com>
2024-03-13 14:38:19 +0200

    Array Task Feedback

--- src/ClassTask/ArraysAndLoops/ArrayTask.java
+++ src/ClassTask/ArraysAndLoops/ArrayTask.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class ArrayTask {
     public static void main(String[] args) {
+        // You needed to use a Scanner. save the 3 numbers in an array and than find the mix and max numbers
         int[] numbers ={15, 30, 45};
         int min = numbers[0];
         int max = numbers[0];
